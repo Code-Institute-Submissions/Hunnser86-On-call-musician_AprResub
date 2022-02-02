341da5cd5b8eba28baf9ed7532c147b0a1e20dc2
Hunnser86 <robhunns@gmail.com>
2022-02-02 11:42:06 +0000

    Customized admin backend

--- services/admin.py
+++ services/admin.py
@@ -1,5 +1,28 @@
+"""
+Import admin and Service
+"""
 from django.contrib import admin
 from .models import Service
 
 # Register your models here.
-admin.site.register(Service)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    """
+    A class to display certain information to the Backend.
+   """
+    list_display = (
+        'name',
+        'description',
+        'price',
+        'frequency',
+        'access',
+        'original_music',
+        'rating',
+        'image',
+    )
+
+    ordering = ('price',)
+
+
+admin.site.register(Service, ServiceAdmin)
