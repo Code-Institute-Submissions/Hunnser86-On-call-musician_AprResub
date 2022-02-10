0a3a430a63a462be186b877d0bd7dcbf33b202c1
Hunnser86 <robhunns@gmail.com>
2022-02-10 10:14:25 +0000

    Added service detail functionality

--- services/views.py
+++ services/views.py
@@ -1,7 +1,7 @@
 """
 Import render and Service model
 """
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Service
 
 
@@ -15,3 +15,15 @@ def all_services(request):
     }
 
     return render(request, 'services/services.html', context)
+
+
+def service_detail(request, service_id):
+    """ A view to show individual services"""
+
+    service = get_object_or_404(Service, pk=service_id)
+
+    context = {
+        'service': service,
+    }
+
+    return render(request, 'services/service_detail.html', context)
