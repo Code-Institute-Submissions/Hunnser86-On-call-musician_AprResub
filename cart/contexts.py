bd92d4bfd9f24e7c6d299599b124dfa6a02b4243
Hunnser86 <robhunns@gmail.com>
2022-02-11 12:27:20 +0000

    updated context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from services.models import Service
 
 def cart_contents(request):
     """ A context processor for the cart items"""
@@ -7,6 +9,17 @@ def cart_contents(request):
     total = 0
     service_count = 0
     discount = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        service = get_object_or_404(Service, pk=item_id)
+        total+= quantity * service.price
+        service_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'service': service,
+        })
 
     if total < settings.TEN_PERCENT_DISCOUNT:
         discount = total * Decimal(settings.SINGLE_PRODUCT_COUNT)
