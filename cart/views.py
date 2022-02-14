3ed2977fdc001f62804abdc2655774f93e0a181b
Hunnser86 <robhunns@gmail.com>
2022-02-14 13:27:14 +0000

    Added CSS for toasts, shopping cart preview and additional messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from services.models import Service
@@ -12,7 +12,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified service to the cart """
 
-    service = Service.objects.get(pk=item_id)
+    service = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -30,14 +30,18 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """ Adjust the quantity of an item in the cart """
 
+    service = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request,
+                         f'Updated {service.name} quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
-
+        messages.success(request,
+                         f'Removed {service.name} from your cart')
     request.session['cart'] = cart
     print(quantity)
     print(cart)
@@ -47,11 +51,14 @@ def adjust_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """ Remove items from the cart """
     try:
+        service = get_object_or_404(Service, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
-
+        messages.success(request,
+                         f'Removed {service.name} from your cart')
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
