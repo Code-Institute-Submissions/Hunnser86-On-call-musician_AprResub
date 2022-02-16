5191b01093b68ad7a5b56e174b791d5fd040da46
Hunnser86 <robhunns@gmail.com>
2022-02-16 09:59:44 +0000

    Added checkout views and templates

--- on_call_musician/urls.py
+++ on_call_musician/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('services/', include('services.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
