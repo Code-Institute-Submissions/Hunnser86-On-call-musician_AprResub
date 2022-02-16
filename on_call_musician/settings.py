bea63acd173aa8273b14008ca06d53eff5474448
Hunnser86 <robhunns@gmail.com>
2022-02-16 11:08:08 +0000

    Added initial checkout functionality

--- on_call_musician/settings.py
+++ on_call_musician/settings.py
@@ -179,8 +179,18 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+#stripe
 TEN_PERCENT_DISCOUNT = 2
 SINGLE_PRODUCT_COUNT = 1
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
