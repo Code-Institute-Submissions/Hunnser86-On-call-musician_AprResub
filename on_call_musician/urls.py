6ba64d9a857ead6c1ba6a3f58231f38f88c13a35
Hunnser86 <robhunns@gmail.com>
2022-02-22 14:37:03 +0000

    Added profiles app

--- on_call_musician/urls.py
+++ on_call_musician/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
