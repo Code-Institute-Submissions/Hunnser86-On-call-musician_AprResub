50e2c30b56cd5d1dc21fcf007fbbfe9d1b9cd3db
Hunnser86 <robhunns@gmail.com>
2022-03-03 19:58:28 +0000

    deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn on-call-musician.wsgi:application
--- on_call_musician/settings.py
+++ on_call_musician/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3&hcebjwgvgl+!^i#s*$^*@ffboj-thu(2z@07(v5dklz&hf6l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['on-call-musician.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -131,12 +131,18 @@ WSGI_APPLICATION = 'on_call_musician.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
