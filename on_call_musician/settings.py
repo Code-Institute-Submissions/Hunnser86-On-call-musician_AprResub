f8cb6122f893224a52a50d467e5b50877239766a
Hunnser86 <robhunns@gmail.com>
2022-04-16 11:17:56 +0000

    Linked the database to heroku app

--- on_call_musician/settings.py
+++ on_call_musician/settings.py
@@ -137,7 +137,7 @@ WSGI_APPLICATION = 'on_call_musician.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        
     }
 else:
     DATABASES = {
@@ -146,6 +146,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+
 
 
 # Password validation
