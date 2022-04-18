22dd71d78e02db27d5ec63d7ea15834f5567bb77
Hunnser86 <robhunns@gmail.com>
2022-04-18 15:28:19 +0000

    Added new layout to music_library.html and add image_url and image to the Music model

--- music_library/models.py
+++ music_library/models.py
@@ -10,6 +10,8 @@ class Music(models.Model):
         verbose_name_plural = 'Music'
 
     name = models.CharField(max_length=254)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     description = models.TextField(max_length=400)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                  blank=True)
