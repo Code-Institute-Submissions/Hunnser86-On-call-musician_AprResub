6bf1d95dd7e666713452dec9f9459ef8eb63d8b4
Hunnser86 <robhunns@gmail.com>
2022-04-18 13:57:12 +0000

    Created a music model in music_library and created fixtures and music.json file

--- music_library/models.py
+++ music_library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Music(models.Model):
+
+    """
+    A class to display the songs for the music library
+
+    """
+    name = models.CharField(max_length=254)
+    description = models.TextField(max_length=400)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+
+    def __str__(self):
+        return self.name
