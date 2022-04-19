4166fed589963bdf3b2e25e000c426b08d95b7dc
Hunnser86 <robhunns@gmail.com>
2022-04-19 09:06:55 +0000

    Added fixtures to the music library

--- music_library/models.py
+++ music_library/models.py
@@ -10,7 +10,7 @@ class Music(models.Model):
     """
     name = models.CharField(max_length=254)
     description = models.TextField(max_length=400)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
