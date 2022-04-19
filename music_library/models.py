b6afef963f65c5dd4981b6502e35c1298296e7eb
Hunnser86 <robhunns@gmail.com>
2022-04-19 08:49:23 +0000

    Added a model to display music in the music library

--- music_library/models.py
+++ music_library/models.py
@@ -1,1 +1,17 @@
+"""
+A model to display music in the music library.
+"""
 from django.db import models
+
+
+class Music(models.Model):
+    """
+    A class to return the correct information to the frontend.
+    """
+    name = models.CharField(max_length=254)
+    description = models.TextField(max_length=400)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
